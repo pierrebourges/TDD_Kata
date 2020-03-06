cc9d3f62552099db9451e58f0b6e6da9bfa6a886
Pierre Bourgès <pierre.bourges@ynov.com>
2020-03-06 14:09:37 +0100

    RED : if the year is not divisible by 400

--- src/__tests__/LeapYears.test.js
+++ src/__tests__/LeapYears.test.js
@@ -3,3 +3,7 @@ const isleapYears = require('../main/LeapYears');
 test('leapYears return true if the parameter is divisible by 400', () =>{
   expect(isleapYears(400)).toBe(true);
 });
+
+test('leapYears return true if the parameter is not divisible by 400', () =>{
+  expect(isleapYears(666)).toBe(false);
+});

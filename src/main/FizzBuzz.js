8b7de8df88d7ee60c7404870480add37aa7366d7
Pierre Bourgès <pierre.bourges@ynov.com>
2020-03-06 12:15:07 +0100

    GREEN : test if the number is divisible by 3

--- src/main/FizzBuzz.js
+++ src/main/FizzBuzz.js
@@ -2,6 +2,9 @@ function fizzBuzz(number){
   if(number % 3 !== 0 && number % 5 !== 0 ){
     return number.toString();
   }
+  if(number % 3 === 0){
+    return "Fizz";
+  }
 }
 
 module.exports = fizzBuzz;

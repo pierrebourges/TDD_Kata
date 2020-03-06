a5e825ca3c65541092bad358c81738a9227f458d
Pierre Bourgès <pierre.bourges@ynov.com>
2020-03-06 12:09:17 +0100

    REFACTOR : test if the number is not devisible by 3 and 5

--- src/__tests__/FizzBuzz.test.js
+++ src/__tests__/FizzBuzz.test.js
@@ -1,5 +1,7 @@
 const fizzBuzz = require('../main/FizzBuzz');
 
 test('fizzBuzz should return the number if the parameter is not divisible by 3 and 5', () => {
-  expect(fizzBuzz(4)).toBe(4);
+  expect(fizzBuzz(1)).toBe("1");
 });
+
+//test('fizzBuzz should return "Fizz')
--- src/main/FizzBuzz.js
+++ src/main/FizzBuzz.js
@@ -1,6 +1,6 @@
 function fizzBuzz(number){
   if(number % 3 !== 0 && number % 5 !== 0 ){
-    return number;
+    return number.toString();
   }
 }
 

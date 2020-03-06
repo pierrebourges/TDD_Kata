2021929db1f7b431a8a0d42c36a9175cfa31eeff
Pierre Bourgès <pierre.bourges@ynov.com>
2020-03-06 12:19:43 +0100

    REFACTOR : test if the number is divisible by3

--- src/main/FizzBuzz.js
+++ src/main/FizzBuzz.js
@@ -1,9 +1,10 @@
 function fizzBuzz(number){
-  if(number % 3 !== 0 && number % 5 !== 0 ){
-    return number.toString();
-  }
   if(number % 3 === 0){
     return "Fizz";
+  }
+
+  if(number % 5 !== 0 ){
+    return number.toString();
   }
 }
 

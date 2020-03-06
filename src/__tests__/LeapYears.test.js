ee43c23982c3c8cacb5bd7810b3bc5ee359f5b88
Pierre Bourgès <pierre.bourges@ynov.com>
2020-03-06 14:19:19 +0100

    Creation test return false for year divisible by 100 but not by 400

--- src/__tests__/LeapYears.test.js
+++ src/__tests__/LeapYears.test.js
@@ -4,6 +4,10 @@ test('leapYears return true if the parameter is divisible by 400', () =>{
   expect(isleapYears(400)).toBe(true);
 });
 
-test('leapYears return true if the parameter is not divisible by 400', () =>{
+test('leapYears return false if the parameter is not divisible by 400', () =>{
   expect(isleapYears(666)).toBe(false);
+});
+
+test('leapYears return false if the parameter is not divisible by 100 but not by 400', () =>{
+  expect(isleapYears(100)).toBe(false);
 });

184637842c28d8ef8acf5084c7da3d34d4481930
Pierre Bourgès <pierre.bourges@ynov.com>
2020-03-06 12:20:39 +0100

    RED : test if the number is divisible by 5

--- src/__tests__/FizzBuzz.test.js
+++ src/__tests__/FizzBuzz.test.js
@@ -7,3 +7,7 @@ test('fizzBuzz should return the number if the parameter is not divisible by 3 a
 test('fizzBuzz should return "Fizz" if the parameter is divisible by 3', () => {
   expect(fizzBuzz(3)).toBe("Fizz");
 });
+
+test('fizzBuzz should return "Buzz" if the parameter is divisible by 5', () => {
+  expect(fizzBuzz(5)).toBe("Buzz");
+});

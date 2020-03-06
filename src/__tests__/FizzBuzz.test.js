4d76fe24b15c995b107e0a4a91132fec0757f932
Pierre Bourgès <pierre.bourges@ynov.com>
2020-03-06 11:54:11 +0100

    RED : test if the number is not divisible by 3 and 5

---
+++ src/__tests__/FizzBuzz.test.js
@@ -0,0 +1,5 @@
+const fizzBuzz = require('../main/FizzBuzz');
+
+test('fizzBuzz should return the number if the parameter is not divisible by 3 and 5', () => {
+  expect(fizzBuzz(1)).toBe("1");
+});

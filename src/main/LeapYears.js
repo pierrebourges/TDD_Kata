10cfddc4086538231219f306310cb02ccfe28b01
Pierre Bourgès <pierre.bourges@ynov.com>
2020-03-06 14:30:10 +0100

    REFACTOR : year is divisible by 100 but not by 400

--- src/main/LeapYears.js
+++ src/main/LeapYears.js
@@ -1,11 +1,10 @@
-function isleapYears(year){
-  if(year % 400 === 0){
+function isleapYears(year) {
+  if (year % 400 === 0) {
     return true;
+  } else if (year % 100 === 0) {
+    return false;
   }
-  if(year % 100 === 0 && year % 400 !==0){
-    return false
-  }
-  return false
+  return false;
 }
 
 module.exports = isleapYears;
